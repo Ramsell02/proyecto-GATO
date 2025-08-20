36476a6995cc468755de1c3a496f335b9501e425
Ramsell02 <ramsell02@hotmail.com>
2025-08-20 15:38:47 -0600

    dia 3

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,7 @@
 const dado=document.getElementById("dados");
 const resultadoDado=document.getElementById("resultadoDado");
 const casillas=document.getElementsByClassName("cuadro")
-
+const estado=document.getElementById("estado")
 function lanzarDado() {
     // numero random entre 1 y 20 
     // el +1 es para que me de el numero 20 sin el solo llegaria a 19
@@ -16,25 +16,90 @@ function lanzarDado() {
         dado.textContent = numero;
         resultadoDado.textContent="resultado:" + numero;
         dado.style.transform =  "rotateX(0deg) rotateY(0deg)"; // reinicia la posicion
+        
     }, 1000);
 
-    return numero
+    return numero;
+    
 }
 
-let jugador = true
+let jugador = true                                          //esta funcion es para que gire el dado y se marquen las casillas por el resultado
 for (let index = 0; index < casillas.length; index++) {
    
     casillas[index].addEventListener("click", function(){
         const numero=lanzarDado()
-        if (numero>10) {
-            casillas[index].textContent = "X"
-        }else{
-            resultadoDado.textContent= "Turno perdido"
-        }
-    })
+        console.log(numero);
+        
+        if (numero>10 && casillas[index].textContent === "") {
 
-    
+            if (jugador) {
+                setTimeout(function() {
+                    casillas[index].textContent = "X"
+                     ganador()
+                }, 2000)
+                jugador = false
+                estado.textContent="turno de O"
+            } else{
+                setTimeout(function() {
+                    casillas[index].textContent = "O"
+                     ganador()
+                }, 2000)
+                jugador = true
+                estado.textContent="turno de X"
+            }
+            
+            
+        }else{
+            setTimeout(function() {
+                estado.textContent= "Turno perdido"
+            }, 2000)
+            if (jugador) {
+                jugador = false
+            } else{
+                jugador = true
+            }
+            
+        }
+       
+    })  
+
 }
 
+
+
+function ganador() {
+    let posicionesGanadoras=[ 
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,4,8],
+        [2,4,6],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8]
+    ]
+
+    for (let index = 0; index < posicionesGanadoras.length; index++) {
+        const element = posicionesGanadoras[index];
+        if (casillas[element[0]].textContent === "X" && casillas[element[1]].textContent === "X" && casillas[element[2]].textContent === "X" ) {
+           setTimeout(function() {
+                alert("gano X")
+            }, 500)
+           
+        }
+        if (casillas[element[0]].textContent === "O" && casillas[element[1]].textContent === "O" && casillas[element[2]].textContent === "O" ) {
+         setTimeout(function() {
+                alert("gano O")
+            }, 500)
+            
+        }
+        
+           
+       
+       
+    }
+
+
+}
 
 
